148681950e38016592e30b5ead9ff20053a16f39
Nithin Pulikkathodiyil <nithume64@yahoo.com>
2026-09-04 12:34:11 +0200

    Add fixture setup and teardown flow

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,10 @@ from playwright.sync_api import Page
 
 @pytest.fixture
 def playwright_home(page: Page):
+    print("\nSETUP: Opening Playwright homepage")
+
     page.goto("https://playwright.dev/")
-    return page
+
+    yield page
+
+    print("\nTEARDOWN: Test has finished")
