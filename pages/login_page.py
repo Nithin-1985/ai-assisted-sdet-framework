a47ba38aac25895b86266c5bcc2e39df44028c35
Nithin Pulikkathodiyil <nithume64@yahoo.com>
2026-09-06 12:30:41 +0200

    Refactor login page to use reusable locators

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,10 +6,14 @@ class LoginPage:
     def __init__(self, page: Page):
         self.page = page
 
+        self.username_input = page.get_by_label("Username")
+        self.password_input = page.get_by_label("Password")
+        self.login_button   = page.get_by_role("button",name = "Login")
+
     def open(self):
         self.page.goto("https://the-internet.herokuapp.com/login")
 
     def login(self, username: str, password: str):
-        self.page.get_by_label("Username").fill(username)
-        self.page.get_by_label("Password").fill(password)
-        self.page.get_by_role("button", name="Login").click()
+        self.username_input.fill(username)
+        self.password_input.fill(password)
+        self.login_button.click()
