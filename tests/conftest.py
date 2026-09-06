2785593f0d5da27f204ab2eebe090f530e8f7bc4
Nithin Pulikkathodiyil <nithume64@yahoo.com>
2026-09-06 12:55:18 +0200

    Add LoginPage pytest fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from playwright.sync_api import Page
+from pages.login_page import LoginPage
 
 
 @pytest.fixture
@@ -11,3 +12,7 @@ def playwright_home(page: Page):
     yield page
 
     print("\nTEARDOWN: Test has finished")
+
+@pytest.fixture
+def login_page(page: Page):
+    return LoginPage(page)
--- tests/test_form.py
+++ tests/test_form.py
@@ -1,9 +1,7 @@
 from playwright.sync_api import Page, expect
 from pages.login_page import LoginPage
 
-
-def test_login_form(page:Page):
-    login_page = LoginPage(page)
+def test_login_form(page: Page, login_page):
 
     login_page.open()
     login_page.login("tomsmith", "SuperSecretPassword!")
@@ -12,8 +10,7 @@ def test_login_form(page:Page):
     expect(page.get_by_text("You logged into a secure area!")).to_be_visible()
 
 
-def test_login_with_invalid_password(page: Page):
-    login_page = LoginPage(page)
+def test_login_with_invalid_password(page: Page,login_page):
 
     login_page.open()
     login_page.login("tomsmith", "wrongpassword")
