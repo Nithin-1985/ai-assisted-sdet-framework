a1ad09c230aa9e8f55b9a7b77a6c20544c71cdf6
Nithin Pulikkathodiyil <nithume64@yahoo.com>
2026-09-12 12:52:12 +0200

    Add logged-in Playwright fixture

--- tests/test_form.py
+++ tests/test_form.py
@@ -20,3 +20,19 @@ def test_login_with_invalid_password(page: Page,login_page):
     login_page.login("tomsmith", "wrongpassword")
 
     expect(page.get_by_text("Your password is invalid!")).to_be_visible()
+
+
+def test_secure_page(logged_in_page):
+
+    expect(logged_in_page).to_have_url(
+        "https://the-internet.herokuapp.com/secure"
+    )
+
+    expect(
+        logged_in_page.get_by_text("You logged into a secure area!")
+    ).to_be_visible()
+
+def test_logout_button_visible(logged_in_page):
+    expect(
+        logged_in_page.get_by_role("link", name="Logout")
+    ).to_be_visible()
